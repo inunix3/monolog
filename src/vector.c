47a589b8ef76157e2e7f2892072623ba55bbeb61
inunix3 <inunix3@tutamail.com>
2025-04-26 15:13:35 +0200

    Add test for vector

--- src/vector.c
+++ src/vector.c
@@ -12,6 +12,11 @@
 
 bool vec_init(Vector *self, size_t element_size) {
     self->data = malloc(VECTOR_DEFAULT_CAP * element_size);
+
+    if (!self->data) {
+        return false;
+    }
+
     self->cap = VECTOR_DEFAULT_CAP;
     self->len = 0;
     self->element_size = element_size;
@@ -25,17 +30,28 @@ void vec_deinit(Vector *self) {
     }
 
     free(self->data);
-
     memset(self, 0, sizeof(*self));
 }
 
-void vec_push(Vector *self, const void *data) {
+bool vec_push(Vector *self, const void *data) {
     if (self->len >= self->cap) {
-        self->data = realloc(self->data, self->cap * self->element_size + VECTOR_DEFAULT_CAP * self->element_size);
+        self->data = realloc(
+            self->data, self->cap * self->element_size +
+                            VECTOR_DEFAULT_CAP * self->element_size
+        );
+
+        if (!self->data) {
+            return false;
+        }
+
         self->cap += VECTOR_DEFAULT_CAP;
     }
 
-    memcpy(self->data + self->len++ * self->element_size, data, self->element_size);
+    memcpy(
+        self->data + self->len++ * self->element_size, data, self->element_size
+    );
+
+    return true;
 }
 
 void vec_pop(Vector *self) {
@@ -43,8 +59,26 @@ void vec_pop(Vector *self) {
         return;
     }
 
-    memset(self->data + (self->len - 1) * self->element_size, 0, self->element_size);
+    memset(
+        self->data + (self->len - 1) * self->element_size, 0, self->element_size
+    );
+
     --self->len;
+}
+
+bool vec_reserve(Vector *self, size_t new_cap) {
+    if (new_cap <= self->cap) {
+        return false;
+    }
+
+    self->data = realloc(
+        self->data,
+        new_cap * self->element_size
+    );
+
+    self->cap = new_cap;
+
+    return true;
 }
 
 void vec_clear(Vector *self) {
---
+++ tests/vector.c
@@ -0,0 +1,220 @@
+#include <monolog/vector.h>
+
+#include <greatest.h>
+
+static Vector g_vec;
+
+#define NTH_VALUE(_i) (((int *) g_vec.data)[_i])
+
+void set_up(void *udata) {
+    (void) udata;
+
+    vec_init(&g_vec, sizeof(int));
+}
+
+void tear_down(void *udata) {
+    (void) udata;
+
+    vec_deinit(&g_vec);
+}
+
+TEST empty_vec(void) {
+    ASSERT_EQ(VECTOR_DEFAULT_CAP, g_vec.cap);
+    ASSERT_EQ(sizeof(int), g_vec.element_size);
+    ASSERT_EQ(0, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    PASS();
+}
+
+TEST push_one_value(void) {
+    int x = 115;
+
+    vec_push(&g_vec, &x);
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP, g_vec.cap);
+    ASSERT_EQ(1, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    ASSERT_EQ(115, NTH_VALUE(0));
+
+    PASS();
+}
+
+TEST push_50_values(void) {
+    for (int i = 0; i < 50; ++i) {
+        vec_push(&g_vec, &i);
+    }
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP, g_vec.cap);
+    ASSERT_EQ(50, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    for (int i = 0; i < 50; ++i) {
+        ASSERT_EQ(i, NTH_VALUE(i));
+    }
+
+    PASS();
+}
+
+TEST grow(void) {
+    for (int i = 0; i < VECTOR_DEFAULT_CAP * 2; ++i) {
+        vec_push(&g_vec, &i);
+    }
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP * 2, g_vec.cap);
+    ASSERT_EQ(VECTOR_DEFAULT_CAP * 2, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    for (int i = 0; i < VECTOR_DEFAULT_CAP * 2; ++i) {
+        ASSERT_EQ(i, NTH_VALUE(i));
+    }
+
+    PASS();
+}
+
+TEST pop_one_value(void) {
+    int x = 115;
+
+    vec_push(&g_vec, &x);
+    vec_pop(&g_vec);
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP, g_vec.cap);
+    ASSERT_EQ(0, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    PASS();
+}
+
+TEST pop_50_values(void) {
+    for (int i = 0; i < 50; ++i) {
+        vec_push(&g_vec, &i);
+    }
+
+    for (int i = 0; i < 50; ++i) {
+        vec_pop(&g_vec);
+    }
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP, g_vec.cap);
+    ASSERT_EQ(0, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    PASS();
+}
+
+TEST clear(void) {
+    for (int i = 0; i < 50; ++i) {
+        vec_push(&g_vec, &i);
+    }
+
+    vec_clear(&g_vec);
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP, g_vec.cap);
+    ASSERT_EQ(0, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    PASS();
+}
+
+TEST capacity_remains_the_same_after_popping_all(void) {
+    for (int i = 0; i < VECTOR_DEFAULT_CAP * 2; ++i) {
+        vec_push(&g_vec, &i);
+    }
+
+    for (int i = 0; i < VECTOR_DEFAULT_CAP * 2; ++i) {
+        vec_pop(&g_vec);
+    }
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP * 2, g_vec.cap);
+    ASSERT_EQ(0, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    PASS();
+}
+
+TEST capacity_remains_the_same_after_clearing(void) {
+    for (int i = 0; i < VECTOR_DEFAULT_CAP * 2; ++i) {
+        vec_push(&g_vec, &i);
+    }
+
+    vec_clear(&g_vec);
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP * 2, g_vec.cap);
+    ASSERT_EQ(0, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    PASS();
+}
+
+TEST custom_capacity(void) {
+    vec_reserve(&g_vec, 300);
+
+    ASSERT_EQ(300, g_vec.cap);
+    ASSERT_EQ(0, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    PASS();
+}
+
+TEST custom_capacity_on_nonempty_vector(void) {
+    for (int i = 0; i < VECTOR_DEFAULT_CAP * 2; ++i) {
+        vec_push(&g_vec, &i);
+    }
+
+    vec_reserve(&g_vec, VECTOR_DEFAULT_CAP * 5);
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP * 5, g_vec.cap);
+    ASSERT_EQ(VECTOR_DEFAULT_CAP * 2, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    for (int i = 0; i < VECTOR_DEFAULT_CAP * 2; ++i) {
+        ASSERT_EQ(i, NTH_VALUE(i));
+    }
+
+    PASS();
+}
+
+TEST custom_capacity_less_than_actual_is_not_applied(void) {
+    for (int i = 0; i < 50; ++i) {
+        vec_push(&g_vec, &i);
+    }
+
+    vec_reserve(&g_vec, 10);
+
+    ASSERT_EQ(VECTOR_DEFAULT_CAP, g_vec.cap);
+    ASSERT_EQ(50, g_vec.len);
+    ASSERT(g_vec.data != NULL);
+
+    for (int i = 0; i < 50; ++i) {
+        ASSERT_EQ(i, NTH_VALUE(i));
+    }
+
+    PASS();
+}
+
+SUITE(vector) {
+    GREATEST_SET_SETUP_CB(set_up, NULL);
+    GREATEST_SET_TEARDOWN_CB(tear_down, NULL);
+
+    RUN_TEST(empty_vec);
+    RUN_TEST(push_one_value);
+    RUN_TEST(push_50_values);
+    RUN_TEST(grow);
+    RUN_TEST(pop_one_value);
+    RUN_TEST(pop_50_values);
+    RUN_TEST(capacity_remains_the_same_after_popping_all);
+    RUN_TEST(capacity_remains_the_same_after_clearing);
+    RUN_TEST(custom_capacity);
+    RUN_TEST(custom_capacity_on_nonempty_vector);
+    RUN_TEST(custom_capacity_less_than_actual_is_not_applied);
+}
+
+GREATEST_MAIN_DEFS();
+
+int main(int argc, char *argv[]) {
+    GREATEST_MAIN_BEGIN();
+
+    RUN_SUITE(vector);
+
+    GREATEST_MAIN_END();
+}
