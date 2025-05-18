b84c32ab6564647acfdf9b782fcd1c9a28a12539
inunix3 <inunix3@tutamail.com>
2025-05-18 05:31:20 +0200

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+gcc -std=gnu23 -fstack-clash-protection -Wall -Wextra -Wpedantic \
+    -Wno-unknown-pragmas -Wno-unused-function -Wno-padded \
+    -Wno-missing-field-initializers -Wno-missing-braces -DNDEBUG -O3 \
+    -I third-party/isocline/include \
+    -I third-party/isocline/src \
+    -I include \
+    third-party/isocline/src/isocline.c \
+    src/*.c -o monolog
+
