ddd5c23c18a3bfa9c1012228d34723e499cdb474
inunix3 <inunix3@tutamail.com>
2025-05-23 18:58:30 +0200

    remove build scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-gcc -std=c11 -fstack-clash-protection -Wall -Wextra -Wpedantic \
-    -Wno-unknown-pragmas -Wno-unused-function -Wno-padded \
-    -Wno-missing-field-initializers -Wno-missing-braces -DNDEBUG -O3 \
-    -I third-party/isocline/include \
-    -I include \
-    third-party/isocline/src/isocline.c \
-    src/*.c -o monolog
-
